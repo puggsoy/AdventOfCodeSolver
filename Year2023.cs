71abd40c01bc1ca9a9a2910a7890cb166ceccd79
Sami Habib <puggsoy@gmail.com>
2023-12-02 18:40:00 +1300

    Day 1 part 2

--- Year2023.cs
+++ Year2023.cs
@@ -12,7 +12,7 @@ namespace AdventOfCodeSolver
 	{
 		public List<IYear.DayDefinition> DayDefinitions { get; private set; } = new List<IYear.DayDefinition>()
 		{
-			new IYear.DayDefinition(1, true, false)
+			new IYear.DayDefinition(1, true, true)
 		};
 
 		public string Solve(int day, int puzzle, string[] input)
@@ -51,6 +51,86 @@ namespace AdventOfCodeSolver
 				bool IsDigit(char c)
 				{
 					return int.TryParse(c.ToString(), out int _);
+				}
+			}
+			else if (puzzle == 2)
+			{
+				string[] digits = new string[]
+				{
+					"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+				};
+
+				int total = 0;
+
+				foreach (string line in input)
+				{
+					int firstDigit = FirstDigit(line);
+					int lastDigit = LastDigit(line);
+					int joined = (firstDigit * 10) + lastDigit;
+
+					total += joined;
+				}
+
+				return total.ToString();
+
+				int FirstDigit(string line)
+				{
+					int lowestIndex = line.Length - 1;
+					int firstDigit = 0;
+
+					for (int i = 0; i < 10; i++)
+					{
+						int intIndex = line.IndexOf(i.ToString());
+
+						if (intIndex >= 0 && intIndex <= lowestIndex)
+						{
+							lowestIndex = intIndex;
+							firstDigit = i;
+						}
+
+						if (i >= digits.Length)
+							continue;
+
+						int stringindex = line.IndexOf(digits[i]);
+
+						if (stringindex >= 0 && stringindex <= lowestIndex)
+						{
+							lowestIndex = stringindex;
+							firstDigit = i + 1;
+						}
+					}
+
+					return firstDigit;
+				}
+
+				int LastDigit(string line)
+				{
+					int highestIndex = 0;
+					int lastDigit = 0;
+
+					for (int i = 0; i < 10; i++)
+					{
+						int intIndex = line.LastIndexOf(i.ToString());
+
+						if (intIndex >= 0 && intIndex >= highestIndex)
+						{
+							highestIndex = intIndex;
+							lastDigit = i;
+						}
+
+						if (i >= digits.Length)
+							continue;
+
+						int stringindex = line.LastIndexOf(digits[i]);
+
+						if (stringindex >= 0 && stringindex >= highestIndex)
+						{
+							highestIndex = stringindex;
+							lastDigit = i + 1;
+						}
+					}
+
+					return lastDigit;
 				}
 			}
 			else
